317aaad8886e84f042e285cc9712d11ed6352099
Yi_Jun <j0989605375@gmail.com>
2020-10-16 10:07:49 +0800

    Project init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <FunTable />
+  </div>
+</template>
+
+<script>
+import FunTable from "./components/FunTable";
+
+export default {
+  name: "App",
+  components: {
+    FunTable,
+  },
+};
+</script>
+
+<style>
+#app {
+}
+</style>
